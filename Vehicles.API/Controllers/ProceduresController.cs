73b221aa54b601d818b25b7a300250d5cd83b601
emilio barrera <barrera_emilio@yahoo.es>
2021-09-21 19:25:51 +0100

    All Crud class procedure already

---
+++ Vehicles.API/Controllers/ProceduresController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using Vehicles.API.Data;
+using Vehicles.API.Data.Entities;
+
+namespace Vehicles.API.Controllers
+{
+    public class ProceduresController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public ProceduresController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dataContext.Procedures.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Procedure procedure)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _dataContext.Add(procedure);
+                    await _dataContext.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe este procedimiento.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+            }
+
+            return View(procedure);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Procedure procedure = await _dataContext.Procedures.FindAsync(id);
+            if (procedure == null)
+            {
+                return NotFound();
+            }
+
+            return View(procedure);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Procedure procedure)
+        {
+
+            if (id != procedure.Id)
+            {
+                return NoContent();
+            }
+
+            if (ModelState.IsValid)
+            {
+
+                try
+                {
+                    _dataContext.Update(procedure);
+                    await _dataContext.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("Duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe el Procedimiento.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+
+            }
+            return View(procedure);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Procedure procedure = await _dataContext.Procedures.FirstOrDefaultAsync(p => p.Id == id);
+            if (procedure == null)
+            {
+                return NotFound();
+            }
+
+            _dataContext.Procedures.Remove(procedure);
+            await _dataContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
